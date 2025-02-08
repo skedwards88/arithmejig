cc285bef9e3707b6245b237d9ba4ea1a408c138b
skedwards88 <skedwards88@gmail.com>
2025-02-08 07:11:47 -0800

    add note

--- src/logic/allEquations.js
+++ src/logic/allEquations.js
@@ -1,5 +1,16 @@
 import {evaluate} from "mathjs";
 
+// Note: I started writing logic to generate equations on the fly based on
+// known elements (see src/logic/generateRandomEquation.js).
+// However, just essentially hard-coding the list like this let me largely reuse
+// my crossjig logic and get to a prototype faster. It also is logically pretty
+// fast to regex for a matching equation instead of generating valid equations
+// on the fly.
+// So I'm not happy with the elegance here, but it makes sense to keep at least
+// for now. In the future, if I let people control the operators and digits that
+// are used, or if I want more complex/diverse expressions, this decision will
+// need to be revisited.
+
 function generateAllEquations() {
   const operators = ["+", "-", "*", "/"];
 
